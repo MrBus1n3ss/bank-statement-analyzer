e09cdfa14aa247e15a5ae0072d9ac9eacbc0d443
John Richardson <59187389+MrBus1n3ss@users.noreply.github.com>
2024-06-19 20:52:26 -0500

    Added csv parser and csv file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    private static final String RESOURCES = "src/main/resources/";
+
+    public static void main(String[] args) throws IOException {
+
+        final Path path = Paths.get(RESOURCES + args[0]);
+
+        final List<String> lines = Files.readAllLines(path);
+        double total = 0d;
+        for(final String line: lines) {
+            final String[] columns = line.split(",");
+            final double amount = Double.parseDouble(columns[1]);
+            total += amount;
+        }
+
+        System.out.println("The total for all transactions is " + total);
     }
 }
